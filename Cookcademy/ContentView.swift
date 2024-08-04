7b42f5af541fed3c35351d9761ba020fd7e1268c
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-08-04 15:46:46 +0930

    Initial Commit

---
+++ Cookcademy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cookcademy
+//
+//  Created by Matthew Morrison on 8/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Matthew Morrison on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
