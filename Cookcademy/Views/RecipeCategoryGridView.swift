97923b09bb07f01593b1b2362795b8c7f5b742e7
Matthew M <145183361+MattMorrison94@users.noreply.github.com>
2024-08-05 19:41:53 +0930

    Relocated recipe data, creted recipecategorygridview

---
+++ Cookcademy/Views/RecipeCategoryGridView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCategoryGridView.swift
+//  Cookcademy
+//
+//  Created by Matthew Morrison on 05/08/2024.
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeCategoryGridView()
+}
